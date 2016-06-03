05bdb08a1befc25529414fde535644ed1d357a9f
Luckygirlllll <irynakanivets@gmail.com>
2016-06-03 18:11:20 +0300

    custom commit

---
+++ app/src/main/java/com/example/attracti/audiorecorderpicture/ViewFragment.java
@@ -0,0 +1,7 @@
+package com.example.attracti.audiorecorderpicture;
+
+/**
+ * Created by attracti on 6/3/16.
+ */
+public class ViewFragment {
+}
---
+++ app/src/main/java/com/example/attracti/audiorecorderpicture/adapterViewProject.java
@@ -0,0 +1,7 @@
+package com.example.attracti.audiorecorderpicture;
+
+/**
+ * Created by attracti on 6/1/16.
+ */
+public class adapterViewProject {
+}
