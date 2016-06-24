118d74419b4ae099bea7e3475f840f706c38870b
Luckygirlllll <irynakanivets@gmail.com>
2016-06-24 16:28:33 +0300

    BottomSheet was added in the first screen.

--- app/src/main/java/com/example/attracti/audiorecorderpicture/activities/FirstscreenActivity.java
+++ app/src/main/java/com/example/attracti/audiorecorderpicture/activities/FirstscreenActivity.java
@@ -1,9 +1,14 @@
 package com.example.attracti.audiorecorderpicture.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,27 +20,31 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.attracti.audiorecorderpicture.R;
+import com.example.attracti.audiorecorderpicture.adapters.ItemAdapter;
 import com.example.attracti.audiorecorderpicture.adapters.RecyclerViewAdapter;
 import com.example.attracti.audiorecorderpicture.model.Folder;
+import com.example.attracti.audiorecorderpicture.model.Item;
 import com.example.attracti.audiorecorderpicture.utils.RecyclerItemClickListener;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Iryna on 5/17/16.
- * <p>
+ * <p/>
  * First screen of the project with RecyclerView  (shows pictures of the all projects)
  */
 
 
-public class FirstscreenActivity extends AppCompatActivity implements RecyclerItemClickListener.OnItemClickListener {
+public class FirstscreenActivity extends AppCompatActivity implements RecyclerItemClickListener.OnItemClickListener,
+        ItemAdapter.ItemListener {
 
     private static final String LOG_TAG = FirstscreenActivity.class.getSimpleName();
 
-    private  LruCache<String, Bitmap> mMemoryCache;
+    private LruCache<String, Bitmap> mMemoryCache;
 
     private RecyclerViewAdapter mAdapter;
     private LinearLayoutManager mLayoutManager;
@@ -53,6 +62,12 @@ public class FirstscreenActivity extends AppCompatActivity implements RecyclerIt
         return mMemoryCache;
     }
 
+    private BottomSheetDialog mBottomSheetDialog;
+    BottomSheetBehavior behavior;
+    private ItemAdapter mAdapterItem;
+    private FloatingActionButton floatButton;
+
+
     public void getFromSdcardFolders() {
         FOLDERS = new ArrayList<>();
         File file = new File(Environment.getExternalStorageDirectory() +
@@ -67,7 +82,7 @@ public class FirstscreenActivity extends AppCompatActivity implements RecyclerIt
                 Log.wtf("List of FOLDERS: ", String.valueOf(listFolders[i].getName()));
 
                 File picturelist = new File(Environment.getExternalStorageDirectory() +
-                        "/Audio_Recorder_Picture/", listFolders[i].getName()+"/Previews");
+                        "/Audio_Recorder_Picture/", listFolders[i].getName() + "/Previews");
                 if (picturelist.isDirectory()) {
                     listFile = picturelist.listFiles();
                     for (int j = 0; j < listFile.length; j++) {
@@ -103,6 +118,39 @@ public class FirstscreenActivity extends AppCompatActivity implements RecyclerIt
         mAdapter = new RecyclerViewAdapter(FirstscreenActivity.this, FOLDERS);
         mList.setAdapter(mAdapter);
         mAdapter.notifyDataSetChanged();
+
+        floatButton = (FloatingActionButton) findViewById(R.id.float_button);
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetDialog();
+                // behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        Log.wtf("bottomSheet==null: ", String.valueOf(bottomSheet==null));
+
+        behavior = BottomSheetBehavior.from(bottomSheet);
+
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                // React to state change
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                // React to dragging events
+            }
+        });
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapterItem = new ItemAdapter(createItems(), this);
+        recyclerView.setAdapter(mAdapterItem);
+
 
         final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
 
@@ -163,14 +211,14 @@ public class FirstscreenActivity extends AppCompatActivity implements RecyclerIt
     public void onItemClick(View childView, int position) {
 
         File picturelist2 = new File(Environment.getExternalStorageDirectory() +
-                "/Audio_Recorder_Picture/", listFolders[position].getName()+"/Pictures");
+                "/Audio_Recorder_Picture/", listFolders[position].getName() + "/Pictures");
         if (picturelist2.isDirectory()) {
             listFile2 = picturelist2.listFiles();
         }
 
         Intent viewScreen = new Intent(getApplicationContext(), ViewActivity.class);
         viewScreen.putExtra("FILE_TAG", listFile2);
-        viewScreen.putExtra("listFile", listFile );
+        viewScreen.putExtra("listFile", listFile);
         startActivity(viewScreen);
 
     }
@@ -178,6 +226,58 @@ public class FirstscreenActivity extends AppCompatActivity implements RecyclerIt
     @Override
     public void onItemLongPress(View childView, int position) {
 
+    }
+
+
+    private void showBottomSheetDialog() {
+        if (behavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
+
+        mBottomSheetDialog = new BottomSheetDialog(this);
+        View view = getLayoutInflater().inflate(R.layout.sheet, null);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ItemAdapter(createItems(), new ItemAdapter.ItemListener() {
+            @Override
+            public void onItemClick(Item item) {
+                if (mBottomSheetDialog != null) {
+                    mBottomSheetDialog.dismiss();
+                }
+            }
+        }));
+
+        mBottomSheetDialog.setContentView(view);
+        mBottomSheetDialog.show();
+        mBottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                mBottomSheetDialog = null;
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdapterItem.setListener(null);
+    }
+
+    public List<Item> createItems() {
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item(R.mipmap.ic_launcher, "Item 1"));
+        items.add(new Item(R.mipmap.ic_launcher, "Item 2"));
+        items.add(new Item(R.mipmap.ic_launcher, "Item 3"));
+        items.add(new Item(R.mipmap.ic_launcher, "Item 4"));
+        return items;
+    }
+
+
+    @Override
+    public void onItemClick(Item item) {
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
     }
 }
 
---
+++ app/src/main/java/com/example/attracti/audiorecorderpicture/model/Item.java
@@ -0,0 +1,23 @@
+package com.example.attracti.audiorecorderpicture.model;
+
+/**
+ * Created by Iryna on 6/24/16.
+ */
+public class Item {
+    private int mDrawableRes;
+
+    private String mTitle;
+
+    public Item(int drawable, String title) {
+        mDrawableRes = drawable;
+        mTitle = title;
+    }
+
+    public int getDrawableResource() {
+        return mDrawableRes;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+}
